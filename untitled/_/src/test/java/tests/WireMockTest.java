5acad8e1e50cabc88bdd70b789a50cb6f281a10c
maxim_yena <nesaje99@gmail.com>
2024-05-12 12:05:30 +0300

    Implement the first test case;

---
+++ untitled/_/src/test/java/tests/WireMockTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.wiremock.Main;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {Main.class})
+@com.github.tomakehurst.wiremock.junit5.WireMockTest
+public class WireMockTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @RegisterExtension
+    static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    @DynamicPropertySource
+    public static void  setUpMockBaseUrl(DynamicPropertyRegistry registry) {
+        registry.add("products_base_url", wireMockExtension::baseUrl);
+    }
+
+    @Test
+    public void testProductSizeIsEmpty() {
+        wireMockExtension.stubFor(
+                WireMock.get("/products")
+                        .willReturn(aResponse()
+                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .withBody("[]")));
+
+        webTestClient.get().uri("api/products/count")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().json("{\"count\": 0}");
+    }
+}
